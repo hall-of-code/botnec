592e38c279e8a2d0c49433d3c06cd0dd65443e02
hall-of-code <m81b4ck2@gmail.com>
2022-04-20 20:07:12 +0200

    await

--- controllers/helptool.py
+++ controllers/helptool.py
@@ -3,7 +3,6 @@ from controllers.admintool import is_admin
 
 
 async def handle(msg, message):
-    await message.delete()
     with open('conf.json', 'r') as f:
         data = json.load(f)
     response = "**-- Help --**\n"
@@ -12,9 +11,10 @@ async def handle(msg, message):
             if data['controllers'][item]['support'][0] != '*':
                 response += '\n**:white_check_mark:  ' + data['controllers'][item]['plugin'].title() + ':**\n' + \
                             data['controllers'][item]['support']
-            elif await is_admin(message):
+            elif is_admin(message) is True:
                 response += '\n**:white_check_mark:  ' + data['controllers'][item]['plugin'].title() + ':**\n' + \
                             data['controllers'][item]['support']
         else:
             response += '\n**:x:  ' + data['controllers'][item]['plugin'].title() + '** -> Zurzeit deaktiviert.\n'
     await message.channel.send(response)
+    await message.delete()
