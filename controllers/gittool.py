00cb6ab6d1e9c75b42a75439cc43de2c7c90b4d9
hall-of-code <m81b4ck2@gmail.com>
2022-04-20 16:22:08 +0200

    await

--- controllers/gittool.py
+++ controllers/gittool.py
@@ -7,6 +7,9 @@ async def handle(msg, message):
     with open('private.json', 'r') as file:
         data = json.load(file)
     name = data['name']
-    if is_admin(message) and (msg[1] == name or msg[1] == '*'):
+    if await is_admin(message) is True and (msg[1] == name or msg[1] == '*'):
         await message.channel.send(f'```\nDer Node {msg[1]} wird gestoppt...\n```')
         exit()
+    else:
+        await message.channel.send(f'```\nDu hast keine Berrechtigungen für diesen Befehl.\n```')
+
