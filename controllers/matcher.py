c06e7463e156879a61571685a243c2ec80b78344
hall-of-code <m81b4ck2@gmail.com>
2022-04-20 19:16:23 +0200

    await

--- controllers/matcher.py
+++ controllers/matcher.py
@@ -5,12 +5,8 @@ import shlex
 
 from controllers.admintool import is_admin
 
-cache_active = ""
 commands = ['/ping', '/lookup', '/help', '/perm', '/track', '/compress', '/git', '/check', '/fallback']
-with open('conf.json', 'r') as file:
-    cache_data = json.load(file)
 
-cache_active = cache_data['active']
 my_name = ""
 with open('private.json', 'r') as n:
     d = json.load(n)
@@ -20,7 +16,9 @@ with open('private.json', 'r') as n:
 async def command_matcher(message):
     msg = shlex.split(message.content.lstrip())
     if message.guild or is_admin(message) is True:
-        global cache_active, my_name
+        with open('conf.json', 'r') as c:
+            dt = json.load(c)
+        cache_active = dt['active']
         if cache_active == my_name:
             for item in commands:
                 if msg[0] == item:
@@ -31,9 +29,4 @@ async def command_matcher(message):
                         await tool.handle(msg, message)
     else:
         await message.channel.send('```\nDie Private Interaktion ist deaktiviert.\n```')
-    if msg[0] == "/fallback" or random.randrange(1,7) == 1:
-        with open('conf.json', 'r') as f:
-            data = json.load(f)
-            global cache_active
-            cache_active = data['active']
 
