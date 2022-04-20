3db0fbe942562a29ceec3404f24a50eb9a6ed50c
hall-of-code <m81b4ck2@gmail.com>
2022-04-20 15:52:02 +0200

    await

--- controllers/checktool.py
+++ controllers/checktool.py
@@ -2,9 +2,9 @@ import json
 
 
 async def handle(msg, message):
-    await message.delete()
     with open('private.json', 'r') as file:
         data = json.load(file)
     name = data['name']
     if msg[1] == name or msg[1] == '*':
+        await message.delete()
         await message.channel.send(f'```[✅] Node "{name}" is running.```')
