1736e070d270abf41b848b612dc5cf11b9ecca8e
hall-of-code <m81b4ck2@gmail.com>
2022-04-20 15:17:05 +0200

    +updater.py

---
+++ updater.py
@@ -0,0 +1,35 @@
+import json
+import os
+import time
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        s = message.content.lstrip().split(' ')
+        command = s[0] or ''
+        value = s[1] or '*'
+        if command == '/git':
+            with open('private.json', 'r') as f:
+                d = json.load(file)
+            if value == d['name'] or value == "*":
+                await message.channel.send(f'```\nDer Node {value} wird gestoppt...\n```')
+                time.sleep(5)
+                os.popen('git pull')
+                time.sleep(5)
+                os.popen('screen -S Botnec python3 main.py')
+                await message.channel.send(f'```\nDer Node {value} wird erneut gestartet.\n```')
+                time.sleep(2.7)
+                await message.channel.send(f'/check {value}')
+
+
+client = MyClient()
+
+with open('private.json', 'r') as file:
+    data = json.load(file)
+client.run(data['token'])
