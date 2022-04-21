94ae58b6b355d0f80a4da613029285c145c523cb
hall-of-code <m81b4ck2@gmail.com>
2022-04-21 12:15:40 +0200

    updater & gittool functionality

--- updater.py
+++ updater.py
@@ -9,6 +9,8 @@ import discord
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        time.sleep(9)
+        await self.change_presence(status=discord.Status.offline)
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
