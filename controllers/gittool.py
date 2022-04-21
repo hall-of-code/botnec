214d94e7a730e1a45b98147918f8dbfb66e047d0
hall-of-code <m81b4ck2@gmail.com>
2022-04-21 11:50:56 +0200

    updater & gittool functionality

--- controllers/gittool.py
+++ controllers/gittool.py
@@ -27,7 +27,7 @@ async def update_updater(msg, message, name):
     try:
         os.popen("screen -X Botnec_Updater quit | grep screen_name | awk '{print $1}' | cut -d. -f1 | xargs kill")
         time.sleep(3)
-        #await message.channel.send(f'```\nDer Updater "UP-{name}" wird erneut gestartet...\n```')
+        # await message.channel.send(f'```\nDer Updater "UP-{name}" wird erneut gestartet...\n```')
         os.popen('screen -S Botnec_Updater -dm python3 updater.py')
     except Exception:
         await message.channel.send(f'```\n[{name}] Es ist ein unerwarteter Fehler aufgetreten.\n```')
