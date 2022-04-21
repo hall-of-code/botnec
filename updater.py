516602946dc7171c3b740a5d141635ada2bff065
hall-of-code <m81b4ck2@gmail.com>
2022-04-21 16:44:10 +0200

    activity time sleep

--- updater.py
+++ updater.py
@@ -58,6 +58,7 @@ class MyClient(discord.Client):
                     await message.delete()
                     await self.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=" -> Ruhemodus"))
                     await self.change_presence(status=discord.Status.offline)
+                    time.sleep(10)
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=" -> Ruhemodus"))
         await self.change_presence(status=discord.Status.offline)
 
