dfab36c03f1ccb5c83865b58c293dbf6426c4fa5
hall-of-code <m81b4ck2@gmail.com>
2022-04-23 19:31:16 +0200

    activity

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 import json
-
 import discord
 from controllers.matcher import command_matcher
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/help"))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        if message.content.lstrip()[0] == '/':
-            await command_matcher(message)  # Match if Command
-
-    async def on_raw_reaction_addhhh(self, reaction):
-        if reaction.emoji.name == '❌':
-            guild = await self.fetch_guild(reaction.guild_id)
-            channel = guild
-            # message = await channel.fetch_message(reaction.message_id)
-            print(channel)
+client = discord.Client()
 
 
-client = MyClient()
+@client.event
+async def on_ready():
+    print('Logged on as {0}!'.format(self.user))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/help"))
+
+
+@client.event
+async def on_message(message):
+    print('Message from {0.author}: {0.content}'.format(message))
+    if message.content.lstrip()[0] == '/':
+        await command_matcher(message)  # Match if Command
+
 
 with open('private.json', 'r') as file:
     data = json.load(file)
